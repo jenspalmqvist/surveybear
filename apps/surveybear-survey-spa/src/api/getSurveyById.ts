c45ca4856a9704c09292303d8ae7a44f40c7b9c4
Giash ITH <89529541+Giash1@users.noreply.github.com>
2023-06-12 13:28:45 +0200

    updating .env och port

--- apps/surveybear-survey-spa/src/api/getSurveyById.ts
+++ apps/surveybear-survey-spa/src/api/getSurveyById.ts
@@ -2,5 +2,5 @@ import { ISurvey } from "@surveybear/surveybear-lib";
 
 export const getSurveyById = async ( surveyId: string): Promise<ISurvey> => {
     console.log(surveyId);
-    return (await fetch(`http://localhost:4200/api/survey/${surveyId}`)).json();
+    return (await fetch(`http://localhost:27017/api/survey/${surveyId}`)).json();
 }
