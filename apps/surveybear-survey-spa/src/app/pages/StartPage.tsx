eccc23f76f4abb78a9e8eeb68a32799b4e193f27
Jens Palmqvist <jpalmqvi@gmail.com>
2023-06-15 10:41:43 +0200

    more fixes

--- apps/surveybear-survey-spa/src/app/pages/StartPage.tsx
+++ apps/surveybear-survey-spa/src/app/pages/StartPage.tsx
@@ -7,19 +7,19 @@ const StartPage = () => {
     const [surveys, setSurveys] = useState<ISurvey[]>([])
 
     useEffect(() => {
-        getSurveys().then(surveys => {
-            setSurveys(surveys);
-        }).catch(e => console.log(e))
+        // getSurveys().then(surveys => {
+        //     setSurveys(surveys);
+        // }).catch(e => console.log(e))
     }, [])
 
 
     return (<>
         <h1>Survey</h1>
         <p>Du är på förstasidan</p>
-        {surveys.map(survey => (
+        {/* {surveys.map(survey => (
             <p><Link to={`/survey/${survey._id?.toString()}`}>{ survey?.recipient.name}</Link></p>
     ))
-}
+} */}
     </>)
 }
 
